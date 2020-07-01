6cdef7c22e722eb257a34ba8411695dfc9d6bf53
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2020-07-01 19:36:30 -0300

    Added translations

--- app/translations/pt/index.js
+++ app/translations/pt/index.js
@@ -1,11 +1,13 @@
 import { formatTranslation } from '../../helpers/translations';
 
+import menu from './menu.json';
 import intro from './intro.json';
 import language from './language.json';
 import form from './form.json';
 
 export default {
   translation: formatTranslation({
+    ...menu,
     ...language,
     ...intro,
     ...form,
