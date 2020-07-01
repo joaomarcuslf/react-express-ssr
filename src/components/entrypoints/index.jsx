e8fb31f0f4d56709829394eac7bbe817c94a403e
João Marcus de Lemos Fernandes <joao.fernandes@wkm.com.br>
2020-07-01 10:00:59 -0300

    Initial Setup

---
+++ src/components/entrypoints/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {hydrate} from "react-dom";
+
+import Index from '../pages/index.jsx'
+
+hydrate(<Index/>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+const config = {
+  entry: {
+    vendor: ["@babel/polyfill", "react"],
+    index: [
+      path.resolve(__dirname, "src", "components", "entrypoints", "index.jsx")
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "src", "public"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+        exclude: [/node_modules/, /public/],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".wasm", ".mjs", "*"],
+  }
+};
+
+module.exports = config;
